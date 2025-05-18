b0f4e98477b53d3c4bcccd35a09a525b0a7e4ade
pythos <yotampeldman@gmail.com>
2025-05-17 18:11:22 -0600

    THIS ONE WORKS

--- inventory/inv_ui.gd
+++ inventory/inv_ui.gd
@@ -1,19 +1,18 @@
 extends Control
 
-@onready var inv:Inv = preload("res://inventory/player_inv.tres")
+#@onready var inv:Inv = preload("res://inventory/player_inv.tres")
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 var is_open = false
 
 
 func _ready():
-	inv.update.connect(update_slots)
-	update_slots()
+#	update_slots()
 	close()
 
-func update_slots():
-	for i in range(min(inv.slot.size(), slots.size())):
-		slots[i].update(inv.slot[i])
+#func update_slots():
+#	for i in range(min(inv.items.size(), slots.size())):
+#		slots[i].update(inv.items[i])
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("i"):
--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -2,13 +2,10 @@ extends Panel
 
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
-@onready var amount_text: Label = $Centercontainer/Panel/Label
 
-func update(slot: InvSlot):
-	if !slot.item:
+func update(item: InvItem):
+	if !item:
 		item_visual.visible = false
 	else:
 		item_visual.visible = true
-		item_visual.texture = slot.item.texture
-		amount_text.visible = true
-		amount_text.text = str(slot.amount)
+		item_visual.texture = item.texture
