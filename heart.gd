0f5f790a10297c87ac15f1b66d99362b245d08ab
pythos <yotampeldman@gmail.com>
2025-05-17 22:42:57 -0600

    working health system and invicbilty

--- heart.gd
+++ heart.gd
@@ -3,21 +3,16 @@ extends HBoxContainer
 var heart_full = preload("res://Assets/brackeys_platformer_assets/2381669-6ade057fd7f23ced.webp")
 var heart_half = preload("res://Assets/brackeys_platformer_assets/2381669-df0942608cf9d9c4.webp")
 var heart_empty = preload("res://Assets/brackeys_platformer_assets/2381669-fac9de5069d05001.webp")
-
 enum TYPES {type1}
 
-
 func update_heart(value):
-
-		
-			update_type1(value)
-
+	update_type1(value)
+	
 func update_type1(value):
 	for i in self.get_child_count():
-		if i*2 <value -1:
-			get_child(i).texture=heart_full
-		elif i*2 ==value -1:
-			get_child(i).texture=heart_half
-	
-		else: get_child(i).texture=heart_empty
-	pass 
+		if i*2 < value - 1:
+			get_child(i).texture = heart_full
+		elif i*2 == value - 1:
+			get_child(i).texture = heart_half
+		else:
+			get_child(i).texture = heart_empty
--- main.gd
+++ main.gd
@@ -1,24 +1,64 @@
 extends Control
 
+class_name HealthController
+
 var current_heart
+var max_heart = 10
+var invincible = false
+var invincible_timer = 0
+var invincible_duration = 3 # Duration of invincibility in seconds
+@onready var heart_display = $HeartContainer # Reference to your HBoxContainer with hearts
+
+# Optional: reference to player sprite for visual feedback during invincibility
+var player_sprite = null
 
 func _ready():
-	current_heart = 6
+	current_heart = max_heart
 	update_heart_num()
-	pass
+	# Add this node to the health_controller group for easy finding
+	add_to_group("health_controller")
 	
+	# Try to find the player sprite for visual feedback
+	if get_parent() and get_parent().has_node("Sprite") or get_parent().has_node("Sprite2D"):
+		player_sprite = get_parent().get_node("Sprite") if get_parent().has_node("Sprite") else get_parent().get_node("Sprite2D")
+
+func _process(delta):
+	# Handle invincibility timer
+	if invincible == true:
+		invincible_timer -= delta
+		
+		# Optional: make the player sprite blink during invincibility
+		if player_sprite:
+			player_sprite.visible = int(invincible_timer * 10) % 2 == 0
+		
+		if invincible_timer <= 0:
+			invincible = false
+			print("Invincibility ended")
+			
+			# Make sure player is visible when invincibility ends
+			if player_sprite:
+				player_sprite.visible = true
+
 func update_heart_num():
-	$"../CharacterBody2D/heart".update_heart(current_heart)
+	# Update the hearts display using the HBoxContainer's update_heart method
+	if heart_display:
+		heart_display.update_heart(current_heart)
+	else:
+		print("ERROR: HeartContainer not found in HealthController!")
 
-func _on_button_pressed() -> void:
+func damage_player(amount):
+	# Check if player is currently invincible
+	if invincible == true:
+		print("Player is invincible - no damage taken")
+	else:
+		current_heart -= 1
+		update_heart_num()
+		print("Player health: ", current_heart)
+		invincible = true
+		invincible_timer = invincible_duration
+		print("Player invincible for ", invincible_duration, " seconds")
 	
-	current_heart +=1
-	update_heart_num()
-	pass # Replace with function body.
-
-
-func _on_button_2_pressed() -> void:
-	
-	current_heart -=1
-	update_heart_num()
-	pass # Replace with function body.
+	# You can add game over logic here
+	if current_heart <= 0:
+		print("GAME OVER")
+		# Implement game over logic here
