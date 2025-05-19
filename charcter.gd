15782bc69096fbd7eabe636ea0bb75984b67e99e
yoyo2kill <yotampeldman@gmail.com>
2025-05-19 09:25:23 -0600

    123

--- charcter.gd
+++ charcter.gd
@@ -9,29 +9,60 @@ const GRAVITY = 980  # Added gravity constant
 @onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
 @onready var FREEZE = preload("res://scence/freeze.tscn")  # Preload the freeze scene
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var following_ui = $FollowingUI
+# Keeps track of the character's facing direction
+var facing_direction = "down"
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-
+	
 	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
 	
-	
+	# Update velocity based on input
+	if direction_x:
+		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	var updown := Input.get_axis("ui_up", "ui_down")
-	if updown:
-		velocity.y = updown * SPEED
+	
+	if direction_y:
+		velocity.y = direction_y * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
+	# Handle animation based on movement
+	update_animation(direction_x, direction_y)
+	
 	move_and_slide()
+
+# New function to handle animations
+func update_animation(direction_x: float, direction_y: float) -> void:	
+	# If character is moving
+	if direction_x != 0 or direction_y != 0:
+		# Determine the primary direction of movement
+		if abs(direction_x) > abs(direction_y):
+			# Moving horizontally
+			if direction_x > 0:
+				animated_sprite_2d.play("walk_right")
+				facing_direction = "right"
+			else:
+				animated_sprite_2d.play("walk_left")
+				facing_direction = "left"
+		else:
+			# Moving vertically
+			if direction_y > 0:
+				animated_sprite_2d.play("walk_down")
+				facing_direction = "down"
+			else:
+				animated_sprite_2d.play("walk_up")
+				facing_direction = "up"
+	else:
+		# Character is idle - play the appropriate idle animation based on facing direction
+		animated_sprite_2d.play("idle_" + facing_direction)
+
 func _input(event: InputEvent) -> void:
 	if texture_progress_bar.value >= 100:
 		if event.is_action_pressed("spell1"):
@@ -41,16 +72,16 @@ func _input(event: InputEvent) -> void:
 		if event.is_action_pressed("spell2"):
 			texture_progress_bar.value -= 200
 			shoot_freeze()
-
 	if event.is_action_pressed("ui_select") and is_ready:
 		dash()
+
 func shoot():
 	var fireball = FIREBALL.instantiate()
-
 	fireball.position = position
 	# Fix direction calculation - this should point FROM player TO mouse
 	fireball.fireball_dir = (get_global_mouse_position() - position).normalized()
 	get_parent().add_child(fireball)
+
 func shoot_freeze():
 	var freeze = FREEZE.instantiate()
 	
@@ -58,26 +89,25 @@ func shoot_freeze():
 	# Same direction calculation as fireball
 	freeze.freeze_dir = (get_global_mouse_position() - position).normalized()
 	get_parent().add_child(freeze)
+
 func dash():
 	$Timer.start()
 	SPEED = 1000
 	is_ready = false
 	coldown_timer.start()
-var is_ready = true
 
+var is_ready = true
 
 func _on_timer_timeout() -> void:
 	SPEED = 300
 
-
 func _on_coldown_timer_timeout() -> void:
 	is_ready = true
-@onready var LightningSpell = preload("res://scence/LightningSpell.tscn")
 
+@onready var LightningSpell = preload("res://scence/LightningSpell.tscn")
 # Spell properties
 @export var lightning_cooldown: float = 2.0
 @export var lightning_mana_cost: int = 0
-
 # Spell state
 var can_cast_lightning = true
 var mana = 100  # Starting mana
@@ -108,3 +138,4 @@ func cast_lightning_spell():
 	# Start cooldown timer
 	var cooldown_timer = get_tree().create_timer(lightning_cooldown)
 	cooldown_timer.timeout.connect(func(): can_cast_lightning = true)
+	
